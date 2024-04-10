551a6848d258837670052f4986aed85c9fed5283
Nicks-Souza <nicolas.souza990@gmail.com>
2024-04-10 14:14:08 -0300

    feat: Adding controller

---
+++ src/br/com/poo/individual/controllers/IndividualController.java
@@ -0,0 +1,10 @@
+package br.com.poo.individual.controllers;
+
+public class IndividualController {
+IndividualController individualController = new IndividualController();
+	
+	//requisição
+	public void individualController() {
+		individualController.individualController();
+	}
+}
